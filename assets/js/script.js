d0e667dc392991d16c1ead79789404ed0c4b371b
enelesmai <xochitl.selene.fp@gmail.com>
2020-02-18 10:04:13 -0600

    changing flex on line 38 to bootstrap classes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,32 @@
 window.onscroll = function () { myFunction() };
 
-var header = document.getElementById("menuHeader");
-var logo = document.getElementById("logoHeader");
-var secondMenu = document.getElementById("secondMenu");
+var header = document.getElementById("header");
+var navHeader = document.getElementById("nav-header");
+var logo = document.getElementById("logo");
+var date = document.getElementById("date");
+var headerTop = document.getElementById("header-top");
+var btnMenu = document.getElementById("btn-menu");
+
 var sticky = header.offsetTop;
 
 function myFunction() {
     if (window.pageYOffset > sticky) {
-        header.classList.add("sticky");
-        logo.classList.add("sticky");
-        logo.classList.add("logo-min-scrolling");
-        secondMenu.classList.add("latest-menu-scrolling");
+        header.classList.add("fixed-top");
+        navHeader.classList.add("d-none");
+        logo.classList.add("brand-fixed");
+        logo.classList.remove("brand");
+        date.classList.add("d-none");
+        headerTop.classList.add("header-top-hfixed");
+        headerTop.classList.remove("header-top-h");
+        btnMenu.classList.remove("d-none");
     } else {
-        header.classList.remove("sticky");
-        logo.classList.remove("sticky");
-        logo.classList.remove("logo-min-scrolling");
-        secondMenu.classList.remove("latest-menu-scrolling");
+        header.classList.remove("fixed-top");
+        navHeader.classList.remove("d-none");
+        logo.classList.remove("brand-fixed");
+        logo.classList.add("brand");
+        date.classList.remove("d-none");
+        headerTop.classList.remove("header-top-hfixed");
+        headerTop.classList.add("header-top-h");
+        btnMenu.classList.add("d-none");
     }
 }
